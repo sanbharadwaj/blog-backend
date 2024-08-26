2b55a26d31b812e808aab2c70c2b56c3cd24a390
sanbharadwaj <sanchit.bharadwaj@outlook.com>
2024-08-26 11:26:09 +0530

    DataModel Update

--- myapp/models.py
+++ myapp/models.py
@@ -1,10 +1,12 @@
 from django.db import models
+from django.utils import timezone
 
 class DummyData(models.Model):
     column1 = models.CharField(max_length=100)
     column2 = models.CharField(max_length=100)
     column3 = models.CharField(max_length=100)
     column4 = models.CharField(max_length=100)
+    created_at = models.DateTimeField(default=timezone.now)
 
     def __str__(self) -> str:
         return self.column1
--- myapp/serializers.py
+++ myapp/serializers.py
@@ -4,4 +4,4 @@ from .models import DummyData
 class DummyDataSerializer(serializers.ModelSerializer):
     class Meta:
         model = DummyData
-        fields = ['column1', 'column2', 'column3', 'column4']
+        fields = ['column1', 'column2', 'column3', 'column4', 'created_at']
